4a1c26dce23f90173ed160fcf0cbe00e17132f6d
mellynaila <mellynaila18@gmail.com>
2026-06-17 21:12:59 +0700

    Project baru

---
+++ database/migrations/2026_06_17_132445_create_peminjaman_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('peminjaman', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('id_anggota')
+                ->constrained('anggota')
+                ->cascadeOnDelete();
+
+            $table->foreignId('id_buku')
+                ->constrained('buku')
+                ->cascadeOnDelete();
+
+            $table->date('tanggal_pinjam');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('peminjaman');
+    }
+};
